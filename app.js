0a4c7437f891ff996342befba43fb3a7317cf9e4
sidgeek <>
2018-09-14 18:54:51 +0800

    first add

---
+++ app.js
@@ -0,0 +1,23 @@
+// APP 入口的 JS
+const Hapi = require('hapi');
+const config = require('./config');
+const routesHelloHapi = require('./routes/hello-hapi');
+
+const server = new Hapi.Server();
+// 配置服务器启动 host 与端口
+server.connection({
+  port: config.port,
+  host: config.host,
+});
+
+const init = async () => {
+  server.route([
+    // 创建一个简单的 hello hapi 接口
+    ...routesHelloHapi,
+  ]);
+  // 启动服务
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+};
+
+init();
